30eb95f58dfd112ab20cc5049b48fc3747069c4c
vkajtazov <kajtazov.viktor@gmail.com>
2015-08-24 22:09:48 +0200

    change

--- src/main/java/com/project/transportMK/web/resources/LineResource.java
+++ src/main/java/com/project/transportMK/web/resources/LineResource.java
@@ -1,15 +1,19 @@
 package com.project.transportMK.web.resources;
 
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.transportMK.model.Line;
-import com.project.transportMK.model.Schedule;
+import com.project.transportMK.model.Station;
+import com.project.transportMK.repository.StationRepository;
 import com.project.transportMK.service.LineService;
+import com.project.transportMK.service.StationService;
 
 @RestController
 @RequestMapping("/data/rest/lines")
@@ -17,16 +21,22 @@ public class LineResource extends CrudResource<Line, LineService> {
 
 	@Autowired
 	private LineService lineService;
+	
+	@Autowired
+	private StationRepository stationRepo;
 
 	@Override
 	public LineService getService() {
 		return lineService;
 	}
 
-	@RequestMapping(value = "/schedules/{startStation}/{endStation}")
-	public List<Schedule> getSchedulesByLine(@PathVariable long startStation,
-			@PathVariable long endStation) {
-		return lineService.findSchedulesByLine(startStation, endStation);
+	@RequestMapping(value = "/schedulesByStations", method = RequestMethod.POST)
+	public String getSchedulesByLine(@RequestParam String startStation,
+			@RequestParam String endStation) throws UnsupportedEncodingException {
+		startStation = URLDecoder.decode(startStation, "UTF-8");
+		endStation = URLDecoder.decode(endStation, "UTF-8");
+		return startStation + " "+endStation;
+		//return lineService.findLineByStations(startStation, endStation);
 	}
 
 }
